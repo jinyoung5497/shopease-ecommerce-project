9ee34be0978d42c96e61c54bb935e19085b67376
jinyoung5497 <jinyoung5497@gmail.com>
2024-10-29 18:52:47 +0900

    feat: design system button component

---
+++ src/packages/button/ButtonProps.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ButtonSize, ButtonVariant, ButtonColor, ButtonRadius } from "./Button";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  color?: ButtonColor;
+  radius?: ButtonRadius;
+  icon?: React.ReactNode;
+  loading?: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
