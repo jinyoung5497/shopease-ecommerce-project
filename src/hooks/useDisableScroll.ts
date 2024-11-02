e10312df89c7c31aca97da2628928483f7acdec7
jinyoung5497 <jinyoung5497@gmail.com>
2024-11-02 10:53:30 +0900

    fix: hide scroll bar & add pull request template

--- src/hooks/useDisableScroll.ts
+++ src/hooks/useDisableScroll.ts
@@ -5,16 +5,16 @@ export const useDisableScroll = (open: boolean) => {
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden"; // 스크롤 비활성화
-      document.body.style.paddingRight = "15px"; // 스크롤 바 너비만큼 오른쪽 패딩 추가
+      // document.body.style.paddingRight = "15px"; // 스크롤 바 너비만큼 오른쪽 패딩 추가
     } else {
       document.body.style.overflow = "auto"; // 스크롤 활성화
-      document.body.style.paddingRight = "0"; // 패딩 복구
+      // document.body.style.paddingRight = "0"; // 패딩 복구
     }
 
     // 컴포넌트 언마운트 시 원래 상태로 복구
     return () => {
       document.body.style.overflow = "auto";
-      document.body.style.paddingRight = "0";
+      // document.body.style.paddingRight = "0";
     };
   }, [open]);
 };
