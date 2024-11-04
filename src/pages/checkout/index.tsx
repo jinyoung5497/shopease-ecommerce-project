30912264ac1ba64879cff4e26258ccccb157f93c
jinyoung5497 <jinyoung5497@gmail.com>
2024-11-04 22:26:48 +0900

    fix: import error

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -10,7 +10,7 @@ import { useAddOrder } from "@/features/order/hooks/useAddOrder";
 import { useDeleteAllCart } from "@/features/cart/hooks/useDeleteCart";
 import { useNavigation } from "@/shared/hooks/useNavigation";
 import OrderInfomation from "./components/OrderInfomation";
-import SubmitInfomation from "./components/submitInfomation";
+import SubmitInfomation from "./components/SubmitInfomations";
 
 const orderSchema = z.object({
   name: z.string().min(1, "주문자명을 입력해주세요."),
