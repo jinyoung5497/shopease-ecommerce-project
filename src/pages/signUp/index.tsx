d8a44f5d82b20d90da605cb1d1af7a4ef6676534
jinyoung5497 <jinyoung5497@gmail.com>
2024-11-04 20:31:26 +0900

    refactor: file structure change

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import google from "../../shared/assets/google.svg";
+import google from "../../../public/assets/google.svg";
 import { useNavigation } from "@/shared/hooks/useNavigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useRegister } from "@/features/auth/api";
