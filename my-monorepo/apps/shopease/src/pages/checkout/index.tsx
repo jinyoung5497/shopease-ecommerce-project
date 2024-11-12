91a2bef9474d3d271bf20357b33c2d026a3688a8
Jinyoung Choi <jinyoung5497@gmail.com>
2024-11-12 20:10:13 +0900

    fix: environment variable name fix

--- my-monorepo/apps/shopease/src/pages/checkout/index.tsx
+++ my-monorepo/apps/shopease/src/pages/checkout/index.tsx
@@ -73,8 +73,8 @@ const Checkout = () => {
       0,
     ) || 0;
 
-  const storeId = import.meta.env.PORTONE_STORE_ID;
-  const channelKey = import.meta.env.PORTONE_CHANNEL_KEY;
+  const storeId = import.meta.env.VITE_PORTONE_STORE_ID;
+  const channelKey = import.meta.env.VITE_PORTONE_CHANNEL_KEY;
 
   const onSubmit: SubmitHandler<FormFields> = useCallback(async () => {
     try {
